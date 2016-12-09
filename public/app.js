590d4a82dd24d6cddd5b36d3eb740b47c810f35a
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-12-09 19:01:28 +0000

    refactoring

--- public/app.js
+++ public/app.js
@@ -4,6 +4,7 @@
   var citiesModel;
   var setSelect;
   var setImageDiv;
+  var mapWrapper;
 
   var setSelectClicked = function( ev ) {
     var selectedSetId = this.selectedOptions[0].setId;
@@ -11,22 +12,26 @@
     console.log( "set selected:", selectedSet );
 
     var setPiecesUrl = "https://rebrickable.com/api/get_set_parts?key=" + apiKey + "&format=json&set=" + selectedSet.id;
-    ajaxHelper.makeGetRequest( setPiecesUrl, function( responseObject ) {
-      console.log( "Recieved pieces for set", selectedSet.id, "(" + selectedSet.name + ")" + ":", responseObject );
+
+    legoModel.getPartsForSet( selectedSetId, function( parts ) {
+      console.log( "Recieved pieces for set", selectedSet.id, "(" + selectedSet.name + ")" + ":", parts );
     });
 
     var setImage = document.querySelector( '#set-image' );
     setImage.src = selectedSet.imageUrl;
   };
 
-  window.onload = function() {
-    console.log( "The hunt for lego has started..." );
-
-    // SET UP MAP
+  var setUpMap = function() {
     var mapContainer = document.getElementById( 'map-container' );
     var center = { lat: 51.5, lng: -0.1227758 };
     var zoom = 10;
-    var mapWrapper = new MapWrapper( mapContainer, center, zoom );
+    mapWrapper = new MapWrapper( mapContainer, center, zoom );
+  }
+
+  window.onload = function() {
+    console.log( "The hunt for lego has started..." );
+
+    // setUpMap();
 
     // LOAD LEGO DATA AND POLULATE SELECT
     ajaxHelper.makeGetRequest( 'data/lego_sets.json', function( responseObject ) {
--- public/models/legoModel.js
+++ public/models/legoModel.js
@@ -10,5 +10,11 @@ LegoModel.prototype = {
     return this.data.find( function( set ) {
       return set.id === setId;
     });
+  },
+  getPartsForSet: function( setId, callback ) {
+    var set = this.getSetWithId( setId );
+    ajaxHelper.makeGetRequest( 'data/example_parts_request.json', function( responseObject ) {
+      callback( responseObject );
+    });
   }
 };
