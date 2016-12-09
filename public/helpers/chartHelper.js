4b40d68d62ac494a0bc15761aa85ec2935fa89fa
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-12-09 21:20:39 +0000

    adding charting of pieces by colour also grouping pieces with small counts in 'other'

--- public/helpers/chartHelper.js
+++ public/helpers/chartHelper.js
@@ -7,22 +7,46 @@ var createChart = function( data, container ) {
     };
   });
 
+  var rawPieceCounts = series.map( function( dataPoint ) {
+    return dataPoint.y;
+  });
+
+  console.log( pieceCounts );
+
+  var maxPieceCount = rawPieceCounts.reduce( function( max, count ) {
+    if ( count > max ) return count;
+    return max;
+  }, 0);
+
+  var pieceCounts = [];
+  var other = 0;
+  series.forEach( function( dataPoint ) {
+    var count = dataPoint.y;
+    count < maxPieceCount / 10 ? other += count : pieceCounts.push( dataPoint );
+  });
+
+  pieceCounts.push({ color: 'OTHER', y: other });
+
+  var categories = pieceCounts.map( function( dataPoint ) {
+    return dataPoint.color;
+  });
+
   console.log( "series:", series );
 
-  // var chart = new Highcharts.Chart({
-  //   chart: {
-  //     type: 'column',
-  //     renderTo: container
-  //   },
-  //   title: {
-  //     text: "test"
-  //   },
-  //   series: [{
-  //     name: "Countries",
-  //     data: series
-  //   }],
-  //   xAxis: {
-  //     categories: categories
-  //   }
-  // });
+  var chart = new Highcharts.Chart({
+    chart: {
+      type: 'column',
+      renderTo: container
+    },
+    title: {
+      text: "Parts Count"
+    },
+    series: [{
+      name: "pieces",
+      data: pieceCounts
+    }],
+    xAxis: {
+      categories: categories
+    }
+  });
 };
