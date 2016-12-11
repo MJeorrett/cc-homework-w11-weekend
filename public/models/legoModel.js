d1672f19691984a28e5293a53537bd45f69b4e85
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-12-11 12:53:28 +0000

    shuffleing parts to randomise colours

--- public/models/legoModel.js
+++ public/models/legoModel.js
@@ -22,7 +22,28 @@ LegoModel.prototype = {
     // });
     ajaxHelper.makeGetRequest( "https://rebrickable.com/api/get_set_parts?key=" + apiKey + "&format=json&set=" + set.id, function( responseObject ) {
       var parts = responseObject[0].parts;
+      parts = shuffle( parts );
       callback( parts );
     });
   }
 };
+
+// taken from http://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+var shuffle = function(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
